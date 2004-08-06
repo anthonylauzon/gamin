387be565caa29791e93c45c836e94ae57e414cf6
Daniel Veillard <veillard@src.gnome.org>
2004-08-06 12:27:27 +0000

    avoid generating zombies Daniel
    * libgamin/gam_fork.c: avoid generating zombies
    Daniel

--- libgamin/gam_fork.c
+++ libgamin/gam_fork.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include "gam_fork.h"
 #include "gam_error.h"
 
@@ -23,15 +25,18 @@ static const char *server_path = BINDIR "/gam_server";
  * Forks and try to launch the server processing the requests for
  * libgamin under the current process id and using the given client ID
  *
- * Returns 0 in case of success or -1 in case of error.
+ * Returns 0 in case of success or -1 in case of detected error.
  */
 int
 gamin_fork_server(const char *fam_client_id)
 {
+    int ret, pid, status;
+
     gam_debug(DEBUG_INFO, "Asking to launch %s with client id %s\n",
               server_path, fam_client_id);
     /* Become a daemon */
-    if (fork() == 0) {
+    pid = fork();
+    if (pid == 0) {
         setsid();
         if (fork() == 0) {
             setenv("GAM_CLIENT_ID", fam_client_id, 0);
@@ -44,5 +49,16 @@ gamin_fork_server(const char *fam_client_id)
 	 */
         _exit(0);
     }
+
+    /*
+     * do a waitpid on the intermediate process to avoid zombies.
+     */
+retry_wait:
+    ret = waitpid (pid, &status, 0);
+    if (ret < 0) {
+        if (errno == EINTR)
+	    goto retry_wait;
+    }
+
     return (0);
 }
