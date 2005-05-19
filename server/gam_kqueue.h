21ba04f8287308e7ac6298088b3dda4836f65a3d
Daniel Veillard <veillard@src.gnome.org>
2005-05-19 20:56:57 +0000

    updated changelog section applied patch for BSD kqueue code from Jean-Yves
    * gamin.spec.in: updated changelog section
    * server/gam_kqueue.c server/gam_kqueue.h: applied patch for BSD
      kqueue code from Jean-Yves Lefort
    Daniel

--- server/gam_kqueue.h
+++ server/gam_kqueue.h
@@ -1,16 +1,8 @@
-
 #ifndef __MD_KQUEUE_H__
 #define __MD_KQUEUE_H__
 
 #include <glib.h>
-#include "gam_poll.h"
 #include "gam_subscription.h"
-
-#define VN_NOTE_MOST	(NOTE_DELETE | NOTE_WRITE | NOTE_EXTEND | \
-			 NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | \
-			 NOTE_REVOKE)
-#define VN_NOTE_ALL	VN_NOTE_MOST
-#define VN_NOTE_CHANGED	(NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK)
 
 G_BEGIN_DECLS
 
