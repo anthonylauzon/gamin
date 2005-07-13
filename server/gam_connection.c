1ae9129df298c9b082aed9bd5a97dad55b143996
Daniel Veillard <veillard@src.gnome.org>
2005-07-13 16:53:54 +0000

    applied patch from Jean-Yves Lefort for compilation on gcc < 3.0 Daniel
    * server/gam_connection.c: applied patch from Jean-Yves Lefort
      for compilation on gcc < 3.0
    Daniel

--- server/gam_connection.c
+++ server/gam_connection.c
@@ -30,10 +30,7 @@ struct GamConnData {
     GMainLoop *loop;            /* the Glib loop used */
     GIOChannel *source;         /* the Glib I/O Channel used */
     int request_len;            /* how many bytes of request are valid */
-    union {
-	GAMPacket request;      /* the next request being read */
-	void *request_data;     /* the next request as a char *  */
-    };
+    GAMPacket request;          /* the next request being read */
     GamListener *listener;      /* the listener associated with the connection */
 };
 
@@ -245,7 +242,7 @@ gam_connection_get_data(GamConnDataPtr conn, char **data, int *size)
     g_assert(data);
     g_assert(size);
 
-    *data = (char *) &conn->request_data + conn->request_len;
+    *data = (char *) &conn->request + conn->request_len;
     *size = sizeof(GAMPacket) - conn->request_len;
 
     return (0);
